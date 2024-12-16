9440489d8d62ca384cd4088e8563403d5237f979
Ignacio Hagopian <jsign.uy@gmail.com>
2024-12-16 17:10:11 -0300

    binary-tree: add Blake3Hasher
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- binary-tree/src/hasher.rs
+++ binary-tree/src/hasher.rs
@@ -8,3 +8,36 @@ pub trait Hasher {
     fn hash_value(value: &[u8; VALUE_LENGTH]) -> Self::Hash;
     fn zero() -> Self::Hash;
 }
+
+pub struct Blake3Hasher;
+
+impl Hasher for Blake3Hasher {
+    type Hash = [u8; 32];
+
+    fn merkelize(a: Self::Hash, b: Self::Hash) -> Self::Hash {
+        let mut input = [0u8; 64];
+        input[..32].copy_from_slice(&a);
+        input[32..].copy_from_slice(&b);
+        if input == [0u8; 64] {
+            return [0u8; 32];
+        }
+        blake3::hash(&input).into()
+    }
+
+    fn hash_value(value: &[u8; VALUE_LENGTH]) -> Self::Hash {
+        blake3::hash(value).into()
+    }
+
+    fn zero() -> Self::Hash {
+        [0u8; 32]
+    }
+
+    fn encode(value: &[u8]) -> Self::Hash {
+        if value.len() > 32 {
+            panic!("Value too long");
+        }
+        let mut out = [0u8; 32];
+        out[..value.len()].copy_from_slice(value);
+        out
+    }
+}

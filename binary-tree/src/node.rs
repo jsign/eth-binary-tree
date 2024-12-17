ca82fc2192c01f7911cd507d5c391a940f2fc41e
Ignacio Hagopian <jsign.uy@gmail.com>
2024-12-17 11:02:48 -0300

    binary-tree: create insert, only covering at EmptyNode
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- binary-tree/src/node.rs
+++ binary-tree/src/node.rs
@@ -17,6 +17,25 @@ pub enum Node {
 }
 
 impl Node {
+    pub fn insert(&mut self, key: Key, value: Value, depth: u8) {
+        match self {
+            Node::Empty => {
+                let mut values = [None; STEM_SUBTREE_WIDTH];
+                values[key.subindex()] = Some(value);
+                *self = Node::Stem {
+                    stem: key.stem(),
+                    values: Box::new(values),
+                };
+            }
+            Node::Stem { stem, values } => {
+                todo!("todo")
+            }
+            Node::Internal { left, right } => {
+                todo!("todo")
+            }
+        }
+    }
+
     fn get_bit(stem: &[u8], depth: u8) -> u8 {
         stem[depth as usize / 8] & (1 << (7 - depth % 8))
     }

55213edda70e9a8b28134a31c350ed7d5d695324
Ignacio Hagopian <jsign.uy@gmail.com>
2024-12-18 20:57:04 -0300

    hash-bench: add Poseidon2 benchmarks
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- hash-bench/benches/hashes.rs
+++ hash-bench/benches/hashes.rs
@@ -1,4 +1,9 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use p3_baby_bear::{BabyBearParameters, Poseidon2BabyBear};
+use p3_koala_bear::{KoalaBearParameters, Poseidon2KoalaBear};
+use p3_mersenne_31::{Mersenne31, Poseidon2Mersenne31};
+use p3_monty_31::MontyField31;
+use p3_symmetric::Permutation;
 use tiny_keccak::Hasher;
 
 pub fn blake3_benchmark(c: &mut Criterion) {
@@ -26,5 +31,29 @@ pub fn keccak_benchmark(c: &mut Criterion) {
     }
 }
 
-criterion_group!(benches, blake3_benchmark, keccak_benchmark);
+pub fn poseidon2_benchmark(c: &mut Criterion) {
+    let mut rng = rand::thread_rng();
+    c.bench_function("poseidon2_KoalaBear_16", |b| {
+        let perm = Poseidon2KoalaBear::<16>::new_from_rng_128(&mut rng);
+        let mut input: [MontyField31<KoalaBearParameters>; 16] = rand::random();
+        b.iter(|| perm.permute_mut(&mut input))
+    });
+    c.bench_function("poseidon2_BabyBear_16", |b| {
+        let perm = Poseidon2BabyBear::<16>::new_from_rng_128(&mut rng);
+        let mut input: [MontyField31<BabyBearParameters>; 16] = rand::random();
+        b.iter(|| perm.permute_mut(&mut input))
+    });
+    c.bench_function("poseidon2_Mersenne31_16", |b| {
+        let perm = Poseidon2Mersenne31::<16>::new_from_rng_128(&mut rng);
+        let mut input: [Mersenne31; 16] = rand::random();
+        b.iter(|| perm.permute_mut(&mut input))
+    });
+}
+
+criterion_group!(
+    benches,
+    blake3_benchmark,
+    keccak_benchmark,
+    poseidon2_benchmark
+);
 criterion_main!(benches);

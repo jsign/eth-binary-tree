76368521794897b6be8b03f15fad27301f805917
Ignacio Hagopian <jsign.uy@gmail.com>
2024-12-20 09:17:27 -0300

    hash-bech: generate random inputs for bench and generalize poseidon2
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- hash-bench/benches/hashes.rs
+++ hash-bench/benches/hashes.rs
@@ -1,52 +1,84 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use p3_baby_bear::{BabyBearParameters, Poseidon2BabyBear};
-use p3_koala_bear::{KoalaBearParameters, Poseidon2KoalaBear};
+use criterion::{criterion_group, criterion_main, BatchSize::SmallInput, Criterion};
+use p3_baby_bear::{BabyBear, Poseidon2BabyBear};
+use p3_field::Field;
+use p3_koala_bear::{KoalaBear, Poseidon2KoalaBear};
 use p3_mersenne_31::{Mersenne31, Poseidon2Mersenne31};
-use p3_monty_31::MontyField31;
 use p3_symmetric::Permutation;
+use rand::{distributions::Standard, prelude::Distribution, RngCore};
 use tiny_keccak::Hasher;
 
 pub fn blake3_benchmark(c: &mut Criterion) {
-    let input = [0x41; 64];
+    let mut rng = rand::thread_rng();
     for size in [32, 64] {
-        let input = &input[..size];
         c.bench_function(&format!("blake3_{}-bytes", size), |b| {
-            b.iter(|| blake3::hash(black_box(input)))
+            b.iter_batched(
+                || {
+                    let mut input = vec![0u8; size];
+                    rng.fill_bytes(&mut input);
+                    input
+                },
+                |input| blake3::hash(&input),
+                SmallInput,
+            )
         });
     }
 }
 
 pub fn keccak_benchmark(c: &mut Criterion) {
-    let input = [0x41; 64];
+    let mut rng = rand::thread_rng();
     for size in [32, 64] {
         c.bench_function(&format!("keccak_{}-bytes", size), |b| {
-            let input = &input[..size];
-            let mut output = [0x00; 32];
-            b.iter(|| {
-                let mut h = tiny_keccak::Keccak::v256();
-                h.update(black_box(input));
-                h.finalize(&mut output);
-            })
+            b.iter_batched(
+                || {
+                    let mut input = vec![0u8; size];
+                    rng.fill_bytes(&mut input);
+                    input
+                },
+                |input| {
+                    let mut h = tiny_keccak::Keccak::v256();
+                    h.update(&input);
+                    h.finalize(&mut [0u8; 32])
+                },
+                SmallInput,
+            )
         });
     }
 }
 
 pub fn poseidon2_benchmark(c: &mut Criterion) {
     let mut rng = rand::thread_rng();
-    c.bench_function("poseidon2_KoalaBear_16", |b| {
-        let perm = Poseidon2KoalaBear::<16>::new_from_rng_128(&mut rng);
-        let mut input: [MontyField31<KoalaBearParameters>; 16] = rand::random();
-        b.iter(|| perm.permute_mut(&mut input))
-    });
-    c.bench_function("poseidon2_BabyBear_16", |b| {
-        let perm = Poseidon2BabyBear::<16>::new_from_rng_128(&mut rng);
-        let mut input: [MontyField31<BabyBearParameters>; 16] = rand::random();
-        b.iter(|| perm.permute_mut(&mut input))
-    });
-    c.bench_function("poseidon2_Mersenne31_16", |b| {
-        let perm = Poseidon2Mersenne31::<16>::new_from_rng_128(&mut rng);
-        let mut input: [Mersenne31; 16] = rand::random();
-        b.iter(|| perm.permute_mut(&mut input))
+    poseidon2_bench::<KoalaBear, _>(
+        c,
+        Poseidon2KoalaBear::<16>::new_from_rng_128(&mut rng),
+        "poseidon2_KoalaBear_16",
+    );
+    poseidon2_bench::<BabyBear, _>(
+        c,
+        Poseidon2BabyBear::<16>::new_from_rng_128(&mut rng),
+        "poseidon2_BabyBear_16",
+    );
+    poseidon2_bench::<Mersenne31, _>(
+        c,
+        Poseidon2Mersenne31::<16>::new_from_rng_128(&mut rng),
+        "poseidon2_Mersenne31_16",
+    );
+}
+
+fn poseidon2_bench<F, Perm>(c: &mut Criterion, perm: Perm, name: &str)
+where
+    F: Field,
+    Perm: Permutation<[F::Packing; 16]>,
+    Standard: Distribution<[F::Packing; 16]>,
+{
+    c.bench_function(name, |b| {
+        b.iter_batched_ref(
+            || {
+                let input: [F::Packing; 16] = rand::random();
+                input
+            },
+            |input| perm.permute_mut(input),
+            SmallInput,
+        );
     });
 }
 
